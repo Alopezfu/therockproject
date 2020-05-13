b694f6aaad4114ad802662633e479952be698c76
Alejandro López <alejandrolopezfuentes789@gmail.com>
2020-05-13 14:05:02 +0200

    fix error 500

--- admin/home.php
+++ admin/home.php
@@ -19,20 +19,14 @@
                 <td>URL</td>
                 <td>Mysql username</td>
                 <td>Phpmyadmin</td>
-<<<<<<< HEAD
                 <td>#</td>
-=======
->>>>>>> 6ef0b0213390bf868e191f13e9e24e45c7ead273
             </thead>
             <tbody>
                 <td><?= $result['suscripcion'] ?></td>
                 <td><?= $result['url'] ?></td>
                 <td><?= $result['mysql_user'] ?></td>
                 <td><?= $result['phpmyadmin'] ?></td>
-<<<<<<< HEAD
                 <td><a href="php/delete.php?id=<?= $result['id'] ?>">Eliminar dominio</a></td>
-=======
->>>>>>> 6ef0b0213390bf868e191f13e9e24e45c7ead273
             </tbody>
         </table>
         <p id="user">Has entrado como <?= $_SESSION['username']; ?> (<a href="php/exit.php">Salir</a>)</p>
