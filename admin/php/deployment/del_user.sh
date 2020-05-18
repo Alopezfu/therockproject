0246866f2a4004b7ad76571c6b038fd281439f35
Alejandro López <alejandrolopezfuentes789@gmail.com>
2020-05-18 13:10:32 +0200

    Update del_user.sh

--- admin/php/deployment/del_user.sh
+++ admin/php/deployment/del_user.sh
@@ -3,7 +3,6 @@
 USERNAME=$1
 WORKDIR='dir_'$USERNAME
 
-rm deployment/$WORKDIR
 cat deployment/deploy.yml > deployment/$WORKDIR/deploy.yml
 
 sed -i 's/alopezfu/'$USERNAME'/g' deployment/$WORKDIR/deploy.yml
