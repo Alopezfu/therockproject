12e1eeb1939f55fe4355f022f3d24b61d1b44fd6
Alejandro López <alejandrolopezfuentes789@gmail.com>
2020-05-18 13:03:29 +0200

    Update add_user.sh

--- admin/php/deployment/add_user.sh
+++ admin/php/deployment/add_user.sh
@@ -8,10 +8,8 @@ cat deployment/deploy.yml > deployment/$WORKDIR/deploy.yml
 
 sed -i 's/alopezfu/'$USERNAME'/g' deployment/$WORKDIR/deploy.yml
 
-<<<<<<< HEAD
 kubectl apply -f deployment/$WORKDIR/deploy.yml
 =======
 sudo -u kube "kubectl apply -f deployment/$WORKDIR/deploy.yml" 
->>>>>>> 6ef0b0213390bf868e191f13e9e24e45c7ead273
 
 #rm -rf deployment/$WORKDIR/
