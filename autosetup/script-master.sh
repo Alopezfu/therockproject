8bac5b3b02bdc809f7cbdd248506435c22fe22da
Alejandro López <alejandrolopezfuentes789@gmail.com>
2020-05-20 11:08:40 +0200

    Update script-master.sh

--- autosetup/script-master.sh
+++ autosetup/script-master.sh
@@ -126,7 +126,7 @@ function cluster(){
     rm joinOut salida
     kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
     kubectl apply -f https://raw.githubusercontent.com/Alopezfu/therockproject/master/traefik/apply-traefik.yml
-    cp -r /root/.kube /var/www/html/php/
+    cp -r /home/kube/.kube /var/www/html/php/
     chown www-data:www-data /var/www/html -R
     chmod 755 /var/www/html -R
 }
