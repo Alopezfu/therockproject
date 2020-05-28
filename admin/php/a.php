b9450b0fb15e4e4a41ad2bdf41b454205c0e94e8
Alejandro López <alejandrolopezfuentes789@gmail.com>
2020-05-28 09:29:07 +0200

    Update a.php

--- admin/php/a.php
+++ admin/php/a.php
@@ -8,6 +8,7 @@ $kubeconfig = "/var/www/html/php/.kube/config";
 putenv("KUBECONFIG=$kubeconfig");
 
 shell_exec("KUBECONFIG=$kubeconfig ; kubectl apply -f ./deployment/deploy_now.yml; sleep 50");
+shell_exec("mkdir /sitios/$_SESSION[username]");
 $deploy = shell_exec("KUBECONFIG=$kubeconfig ; kubectl describe pod $_SESSION[username] | head -1 | xargs | cut -d ' ' -f 2");
 $deploy=trim($deploy);
 $output = shell_exec("KUBECONFIG=$kubeconfig ; kubectl logs $deploy | grep 'mysql -u' | xargs | cut -d ' ' -f 3 | cut -c 3-112");
